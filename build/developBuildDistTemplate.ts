1a41a64942af4651740f2a64250404a92d120455
yakisova41 <yakisoba5emon@gmail.com>
2022-10-23 09:15:18 +0000

    a

--- build/developBuildDistTemplate.ts
+++
@@ -1,9 +0,0 @@
-(()=>{
-    const webSocket = new WebSocket("ws://localhost:5001");
-    webSocket.onmessage = (event)=>{
-        if(event.data === "reload"){
-            window.location.reload();
-        }
-    }
-})()
-
