f46be254ae269900d1d0474f5f958c9a0fc3a5f4
yakisova41 <yakisoba5emon@gmail.com>
2023-02-04 23:40:18 +0000

    Add コメントを追加 undefinedFilterをimport

--- src/pages/tags/tags.ts
+++ src/pages/tags/tags.ts
@@ -2,8 +2,13 @@ import { search } from "../../pixivApi";
 import { get, Users } from "../../configOperator";
 import { PixivFilter } from "../../filter/PixivFilter";
 import tagFilter from "../../filter/tagFilter";
+import undefinedFilter from "../../filter/undefinedFilter";
 import renderingTagsRoot from "./renderingTagsRoot";
 
+/**
+ * queryを取得
+ * @returns query
+ */
 function getQuery() {
     let query = {};
     location.search
@@ -20,6 +25,12 @@ function getQuery() {
     return { pageNumber, mode, order };
 }
 
+/**
+ * usersがfalse出ない時はusers + "users"を返します
+ * falseであるときは""を返します
+ * @param users usersのモード
+ * @returns usersText
+ */
 function getUsersText(users: false | Users) {
     let text = "";
 
@@ -30,6 +41,11 @@ function getUsersText(users: false | Users) {
     return text;
 }
 
+/**
+ * 検索をした後、絞り込みを行います
+ * @param searchKeyword 検索ワード
+ * @returns 絞り込みのresult
+ */
 async function searchSort(searchKeyword: string) {
     const { pageNumber, mode, order } = getQuery();
     const { users } = get();
@@ -41,15 +57,21 @@ async function searchSort(searchKeyword: string) {
         mode,
         order
     );
+
     const pixivfilter = new PixivFilter();
     pixivfilter.addFilter(tagFilter);
-    const sortResult = pixivfilter.run(searchResponse.body.illustManga.data);
+    pixivfilter.addFilter(undefinedFilter);
 
+    const sortResult = pixivfilter.run(searchResponse.body.illustManga.data);
     return sortResult;
 }
 
 function tags(searchKeyword: string) {
     if (get().block) {
+        /**
+         * 絞り込みの結果をreactのレンダリングイベントに発火します。
+         * @param searchKeyword
+         */
         const renderingWithSearchData = (searchKeyword: string) => {
             searchSort(searchKeyword).then((sortResult) => {
                 document.dispatchEvent(
@@ -60,17 +82,59 @@ function tags(searchKeyword: string) {
             });
         };
 
+        /**
+         * ページ読み込み時
+         */
         setTimeout(() => {
             renderingTagsRoot();
             renderingWithSearchData(searchKeyword);
-        });
+        }, 100);
 
-        document.addEventListener("pixiv-tag-filter-searchChange", () => {
+        /**
+         * queryが変更された時
+         */
+        const searchChangeListener = () => {
+            console.log("search change");
             renderingWithSearchData(searchKeyword);
-        });
-        document.addEventListener("pixiv-tag-filter-config-update", () => {
+        };
+        document.addEventListener(
+            "pixiv-tag-filter-searchChange",
+            searchChangeListener
+        );
+
+        /**
+         * configが変更された時
+         */
+        const configUpdateListener = () => {
+            console.log("config update");
             renderingWithSearchData(searchKeyword);
-        });
+        };
+        document.addEventListener(
+            "pixiv-tag-filter-config-update",
+            configUpdateListener
+        );
+
+        /**
+         * pageChangeのイベント時、すべてのイベントリスナーを放棄
+         */
+        const pageChangeListener = () => {
+            document.removeEventListener(
+                "pixiv-tag-filter-searchChange",
+                searchChangeListener
+            );
+            document.removeEventListener(
+                "pixiv-tag-filter-config-update",
+                configUpdateListener
+            );
+            document.removeEventListener(
+                "pixiv-tag-filter-pageChange",
+                pageChangeListener
+            );
+        };
+        document.addEventListener(
+            "pixiv-tag-filter-pageChange",
+            pageChangeListener
+        );
     }
 }
 
