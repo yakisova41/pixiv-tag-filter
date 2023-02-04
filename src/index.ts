7e5ab7d8ac927907689cc02f1d9716004b1ad919
yakisova41 <yakisoba5emon@gmail.com>
2023-02-04 23:44:41 +0000

    Add compatibleConfig()を追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import addingOptionScreen from "./addingOptionScreen";
 import tags from "./pages/tags/tags";
 import pageChangeObserver, { PageChangeEvent } from "./pageChangeObserver";
+import compatibleConfig from "./compatibleConfig";
 
 function main(): void {
+    compatibleConfig();
     addingOptionScreen();
     pageChangeObserver();
 
