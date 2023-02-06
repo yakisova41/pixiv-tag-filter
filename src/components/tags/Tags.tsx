e4c6a1ddc83da1c2e094ff020b062198a22687e1
yakisova41 <yakisoba5emon@gmail.com>
2023-02-06 12:24:02 +0000

    :dizzy: すべてのイラストをレンダリングするまで表示しない
    useLayoutEffectまでイラスト達を表示しないことにより、バラバラに表示されるのを防ぐ

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import TagsContainer from "./TagsContainer";
 import { illust } from "../../pixivApi";
 import { useRouter } from "../Router";
 import { RouterContext } from "../Router";
+import NotFound from "./NotFound";
 
 const Tags = () => {
     const [illusts, setillusts] = useState<illust[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const router = useRouter();
 
@@ -13,13 +15,28 @@ const Tags = () => {
         "pixiv-tag-filter-rendering-tags",
         (e: CustomEvent<illust[]>) => {
             setillusts(e.detail);
+            setLoading(true);
         }
     );
 
+    useLayoutEffect(() => {
+        setLoading(false);
+    });
+
     return (
-        <RouterContext.Provider value={router}>
-            <TagsContainer illusts={illusts} />
-        </RouterContext.Provider>
+        <>
+            {!loading && (
+                <>
+                    {illusts.length === 0 ? (
+                        <NotFound />
+                    ) : (
+                        <RouterContext.Provider value={router}>
+                            <TagsContainer illusts={illusts} />
+                        </RouterContext.Provider>
+                    )}
+                </>
+            )}
+        </>
     );
 };
 
