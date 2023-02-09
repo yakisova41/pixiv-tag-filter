7d8a855bba1eef7a0d40a6300395e72f1cbe7f3d
yakisova41 <yakisoba5emon@gmail.com>
2023-02-09 12:32:38 +0000

    :art: イラスト読み込みとloading処理をIllustsに移動

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -1,42 +1,15 @@
-import React, { useLayoutEffect, useState } from "react";
-import TagsContainer from "./TagsContainer";
-import { illust } from "../../pixivApi";
+import React from "react";
 import { useRouter } from "../Router";
 import { RouterContext } from "../Router";
-import NotFound from "./NotFound";
+import Illusts from "./Illusts";
 
 const Tags = () => {
-    const [illusts, setillusts] = useState<illust[]>([]);
-    const [loading, setLoading] = useState(true);
-
     const router = useRouter();
 
-    document.addEventListener(
-        "pixiv-tag-filter-rendering-tags",
-        (e: CustomEvent<illust[]>) => {
-            setillusts(e.detail);
-            setLoading(true);
-        }
-    );
-
-    useLayoutEffect(() => {
-        setLoading(false);
-    });
-
     return (
-        <>
-            {!loading && (
-                <>
-                    {illusts.length === 0 ? (
-                        <NotFound />
-                    ) : (
-                        <RouterContext.Provider value={router}>
-                            <TagsContainer illusts={illusts} />
-                        </RouterContext.Provider>
-                    )}
-                </>
-            )}
-        </>
+        <RouterContext.Provider value={router}>
+            <Illusts />
+        </RouterContext.Provider>
     );
 };
 
