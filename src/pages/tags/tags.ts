c57ad37d5b16cfd7ed72e674a8b38db69e244965
yakisova41 <yakisoba5emon@gmail.com>
2023-02-09 12:15:58 +0000

    :truck: getQuery()をgetSearchQuery.tsへ移動　filterをnewするのを一回に

--- src/pages/tags/tags.ts
+++ src/pages/tags/tags.ts
@@ -1,94 +1,49 @@
-import { search } from "../../pixivApi";
-import { get, Users } from "../../configOperator";
+import { search } from "../../utils/pixivApi";
+import { get } from "../../utils/configOperator";
 import { PixivFilter } from "../../filter/PixivFilter";
 import tagFilter from "../../filter/tagFilter";
 import undefinedFilter from "../../filter/undefinedFilter";
 import renderingTagsRoot from "./renderingTagsRoot";
-
-/**
- * queryを取得
- * @returns query
- */
-function getQuery() {
-    let query = {};
-    location.search
-        .split("?")[1]
-        ?.split("&")
-        .map((search) => {
-            const splited = search.split("=");
-            query[splited[0]] = splited[1];
-        });
-    const pageNumber = query.hasOwnProperty("p") ? query["p"] : 1;
-    const mode = query.hasOwnProperty("mode") ? query["mode"] : "all";
-    const order = query.hasOwnProperty("order") ? query["order"] : "date_d";
-
-    return { pageNumber, mode, order };
-}
-
-/**
- * usersがfalse出ない時はusers + "users"を返します
- * falseであるときは""を返します
- * @param users usersのモード
- * @returns usersText
- */
-function getUsersText(users: false | Users) {
-    let text = "";
-
-    if (users !== false) {
-        text = users + "users";
-    }
-
-    return text;
-}
-
-/**
- * 検索をした後、絞り込みを行います
- * @param searchKeyword 検索ワード
- * @returns 絞り込みのresult
- */
-async function searchSort(searchKeyword: string) {
-    const { pageNumber, mode, order } = getQuery();
-    const { users } = get();
-
-    const usersText = getUsersText(users);
-    const searchResponse = await search(
-        searchKeyword + " " + usersText,
-        pageNumber,
-        mode,
-        order
-    );
-
-    const pixivfilter = new PixivFilter();
-    pixivfilter.addFilter(tagFilter);
-    pixivfilter.addFilter(undefinedFilter);
-
-    const sortResult = pixivfilter.run(searchResponse.body.illustManga.data);
-    return sortResult;
-}
+import { getSearchQuery } from "../../utils/getSearchQuery";
 
 function tags(searchKeyword: string) {
     if (get().block) {
+        const filter = new PixivFilter();
+        filter.addFilter(tagFilter);
+        filter.addFilter(undefinedFilter);
+
         /**
          * 絞り込みの結果をreactのレンダリングイベントに発火します。
          * @param searchKeyword
          */
-        const renderingWithSearchData = (searchKeyword: string) => {
-            searchSort(searchKeyword).then((sortResult) => {
-                document.dispatchEvent(
-                    new CustomEvent("pixiv-tag-filter-rendering-tags", {
-                        detail: sortResult,
-                    })
-                );
-            });
+        const renderingWithSearchData = async (searchKeyword: string) => {
+            const { pageNumber, mode, order } = getSearchQuery();
+            const { users } = get();
+
+            const usersText = users === false ? "" : users + "users";
+            const searchResponse = await search(
+                searchKeyword + " " + usersText,
+                pageNumber,
+                mode,
+                order
+            );
+
+            const sortResult = filter.run(searchResponse.body.illustManga.data);
+
+            document.dispatchEvent(
+                new CustomEvent("pixiv-tag-filter-fetch", {
+                    detail: sortResult,
+                })
+            );
         };
 
         /**
          * ページ読み込み時
          */
-        setTimeout(() => {
-            renderingTagsRoot();
+        setTimeout(async () => {
+            await renderingTagsRoot();
             renderingWithSearchData(searchKeyword);
-        }, 100);
+        });
 
         /**
          * queryが変更された時
