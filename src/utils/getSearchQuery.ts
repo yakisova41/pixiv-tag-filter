07ff92704cb5f67020ff1760b79371d8d32469b2
yakisova41 <yakisoba5emon@gmail.com>
2023-02-20 10:19:53 +0000

    🐛 暗黙の型変換を修正

--- src/utils/getSearchQuery.ts
+++ src/utils/getSearchQuery.ts
@@ -11,7 +11,7 @@ export function getSearchQuery() {
             const splited = search.split("=");
             query[splited[0]] = splited[1];
         });
-    const pageNumber = query.hasOwnProperty("p") ? query["p"] : 1;
+    const pageNumber = query.hasOwnProperty("p") ? Number(query["p"]) : 1;
     const mode = query.hasOwnProperty("mode") ? query["mode"] : "all";
     const order = query.hasOwnProperty("order") ? query["order"] : "date_d";
 
