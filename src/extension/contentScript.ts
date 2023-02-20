7e1872a3f52d955d5e89070b229c4728997d3c80
yakisova41 <yakisoba5emon@gmail.com>
2023-02-20 10:24:38 +0000

    ✨ 拡張機能対応 #15

--- src/extension/contentScript.ts
+++ src/extension/contentScript.ts
@@ -1,3 +1,4 @@
-import { main } from "../index";
-
-main();
+const head = document.head;
+const script = document.createElement("script");
+script.src = chrome.runtime.getURL("embed.js");
+head.appendChild(script);
