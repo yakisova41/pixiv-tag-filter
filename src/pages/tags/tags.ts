208c571f315648dbf393d8996371334754fd051f
yakisova41 <yakisoba5emon@gmail.com>
2023-09-09 11:38:46 +0000

    ♻️ インデント数 4 -> 2

--- src/pages/tags/tags.ts
+++ src/pages/tags/tags.ts
@@ -7,169 +7,169 @@ import renderingTagsRoot from "./renderingTagsRoot";
 import { getSearchQuery } from "../../utils/getSearchQuery";
 import { setCache, getCache } from "./tagsCache";
 import {
-    PageChangeEvent,
-    SearchChangeEvent,
+  PageChangeEvent,
+  SearchChangeEvent,
 } from "../../utils/pageChangeObserver";
 
 function tags(searchKeyword: string, pathdata: PageChangeEvent) {
-    const { block } = get();
+  const { block } = get();
 
-    if (block) {
-        const filter = new PixivFilter();
-        filter.addFilter(tagFilter);
-        filter.addFilter(undefinedFilter);
+  if (block) {
+    const filter = new PixivFilter();
+    filter.addFilter(tagFilter);
+    filter.addFilter(undefinedFilter);
 
-        /**
-         * 絞り込みの結果をreactのレンダリングイベントに発火します。
-         * @param searchKeyword
-         */
-        const renderingWithSearchData = async (searchKeyword: string) => {
-            const { pageNumber, mode, order } = getSearchQuery();
-            const { users, blocklist } = get();
+    /**
+     * 絞り込みの結果をreactのレンダリングイベントに発火します。
+     * @param searchKeyword
+     */
+    const renderingWithSearchData = async (searchKeyword: string) => {
+      const { pageNumber, mode, order } = getSearchQuery();
+      const { users, blocklist } = get();
 
-            const usersText = users === false ? "" : users + "users";
-            const searchResponse = await search(
-                searchKeyword +
-                    " " +
-                    usersText +
-                    " " +
-                    blocklist.map((blocktag) => "-" + blocktag).join(" "),
-                pageNumber,
-                mode,
-                order
+      const usersText = users === false ? "" : users + "users";
+      const searchResponse = await search(
+        searchKeyword +
+          " " +
+          usersText +
+          " " +
+          blocklist.map((blocktag) => "-" + blocktag).join(" "),
+        pageNumber,
+        mode,
+        order
+      );
+
+      const sortResult = filter.run(searchResponse.body.illustManga.data);
+
+      setCache(sortResult);
+
+      document.dispatchEvent(
+        new CustomEvent("pixiv-tag-filter-fetch", {
+          detail: sortResult,
+        })
+      );
+    };
+
+    /**
+     * ページ読み込み時
+     */
+    setTimeout(async () => {
+      await renderingTagsRoot(pathdata);
+      /**
+       * tags->artworks->tags時のキャッシュ処理
+       */
+      if (pathdata.before.split("/")[1] === "artworks") {
+        let isScroll = false;
+        const cache = getCache();
+
+        if (cache !== undefined) {
+          const renderListener = () => {
+            isScroll = true;
+            setTimeout(() => {
+              window.scroll({
+                top: cache.scroll,
+              });
+            }, 1);
+            document.removeEventListener(
+              "pixiv-tag-filter-render-success",
+              renderListener
             );
+          };
+          document.addEventListener(
+            "pixiv-tag-filter-render-success",
+            renderListener
+          );
 
-            const sortResult = filter.run(searchResponse.body.illustManga.data);
-
-            setCache(sortResult);
-
-            document.dispatchEvent(
+          if (cache.status === "OK") {
+            setTimeout(() => {
+              document.dispatchEvent(
                 new CustomEvent("pixiv-tag-filter-fetch", {
-                    detail: sortResult,
+                  detail: cache.data,
                 })
-            );
-        };
+              );
+              setCache(cache.data);
 
-        /**
-         * ページ読み込み時
-         */
-        setTimeout(async () => {
-            await renderingTagsRoot(pathdata);
-            /**
-             * tags->artworks->tags時のキャッシュ処理
-             */
-            if (pathdata.before.split("/")[1] === "artworks") {
-                let isScroll = false;
-                const cache = getCache();
-
-                if (cache !== undefined) {
-                    const renderListener = () => {
-                        isScroll = true;
-                        setTimeout(() => {
-                            window.scroll({
-                                top: cache.scroll,
-                            });
-                        }, 1);
-                        document.removeEventListener(
-                            "pixiv-tag-filter-render-success",
-                            renderListener
-                        );
-                    };
-                    document.addEventListener(
-                        "pixiv-tag-filter-render-success",
-                        renderListener
-                    );
-
-                    if (cache.status === "OK") {
-                        setTimeout(() => {
-                            document.dispatchEvent(
-                                new CustomEvent("pixiv-tag-filter-fetch", {
-                                    detail: cache.data,
-                                })
-                            );
-                            setCache(cache.data);
-
-                            const interval = setInterval(() => {
-                                if (isScroll) {
-                                    window.scroll({
-                                        top: cache.scroll,
-                                    });
-                                    clearInterval(interval);
-                                }
-                            });
-                        });
-                    }
-
-                    if (cache.status === "TIMEOUT") {
-                        await renderingWithSearchData(searchKeyword);
-
-                        const interval = setInterval(() => {
-                            if (isScroll) {
-                                window.scroll({
-                                    top: cache.scroll,
-                                });
-                                clearInterval(interval);
-                            }
-                        });
-                    }
-                } else {
-                    renderingWithSearchData(searchKeyword);
+              const interval = setInterval(() => {
+                if (isScroll) {
+                  window.scroll({
+                    top: cache.scroll,
+                  });
+                  clearInterval(interval);
                 }
-            } else {
-                renderingWithSearchData(searchKeyword);
-            }
-        });
+              });
+            });
+          }
 
-        /**
-         * queryが変更された時
-         */
-        const searchChangeListener = ({
-            detail,
-        }: CustomEvent<SearchChangeEvent>) => {
-            if (detail.before.path.split("/")[1] !== "artworks") {
-                console.log("search change");
-                renderingWithSearchData(searchKeyword);
-            }
-        };
-        document.addEventListener(
-            "pixiv-tag-filter-searchChange",
-            searchChangeListener
-        );
+          if (cache.status === "TIMEOUT") {
+            await renderingWithSearchData(searchKeyword);
 
-        /**
-         * configが変更された時
-         */
-        const configUpdateListener = () => {
-            console.log("config update");
-            renderingWithSearchData(searchKeyword);
-        };
-        document.addEventListener(
-            "pixiv-tag-filter-config-update",
-            configUpdateListener
-        );
+            const interval = setInterval(() => {
+              if (isScroll) {
+                window.scroll({
+                  top: cache.scroll,
+                });
+                clearInterval(interval);
+              }
+            });
+          }
+        } else {
+          renderingWithSearchData(searchKeyword);
+        }
+      } else {
+        renderingWithSearchData(searchKeyword);
+      }
+    });
 
-        /**
-         * pageChangeのイベント時、すべてのイベントリスナーを放棄
-         */
-        const pageChangeListener = () => {
-            document.removeEventListener(
-                "pixiv-tag-filter-searchChange",
-                searchChangeListener
-            );
-            document.removeEventListener(
-                "pixiv-tag-filter-config-update",
-                configUpdateListener
-            );
-            document.removeEventListener(
-                "pixiv-tag-filter-pageChange",
-                pageChangeListener
-            );
-        };
-        document.addEventListener(
-            "pixiv-tag-filter-pageChange",
-            pageChangeListener
-        );
-    }
+    /**
+     * queryが変更された時
+     */
+    const searchChangeListener = ({
+      detail,
+    }: CustomEvent<SearchChangeEvent>) => {
+      if (detail.before.path.split("/")[1] !== "artworks") {
+        console.log("search change");
+        renderingWithSearchData(searchKeyword);
+      }
+    };
+    document.addEventListener(
+      "pixiv-tag-filter-searchChange",
+      searchChangeListener
+    );
+
+    /**
+     * configが変更された時
+     */
+    const configUpdateListener = () => {
+      console.log("config update");
+      renderingWithSearchData(searchKeyword);
+    };
+    document.addEventListener(
+      "pixiv-tag-filter-config-update",
+      configUpdateListener
+    );
+
+    /**
+     * pageChangeのイベント時、すべてのイベントリスナーを放棄
+     */
+    const pageChangeListener = () => {
+      document.removeEventListener(
+        "pixiv-tag-filter-searchChange",
+        searchChangeListener
+      );
+      document.removeEventListener(
+        "pixiv-tag-filter-config-update",
+        configUpdateListener
+      );
+      document.removeEventListener(
+        "pixiv-tag-filter-pageChange",
+        pageChangeListener
+      );
+    };
+    document.addEventListener(
+      "pixiv-tag-filter-pageChange",
+      pageChangeListener
+    );
+  }
 }
 
 export default tags;
