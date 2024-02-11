148d816d4c54c09d52a655b68ac352bbb49f9041
yakisova41 <yakisoba5emon@gmail.com>
2024-02-11 15:17:56 +0000

    🌐 Fix: fix translate

--- src/components/optionScreen/options/FilterToggle.tsx
+++ src/components/optionScreen/options/FilterToggle.tsx
@@ -4,31 +4,31 @@ import { set, get } from "../../../utils/configOperator";
 import { translate } from "../../../utils/translate";
 
 const FilterToggle = () => {
-    const [value, setValue] = useState(false);
+  const [value, setValue] = useState(false);
 
-    useEffect(() => {
-        const storageValue = get().block;
-        setValue(storageValue);
-    }, []);
+  useEffect(() => {
+    const storageValue = get().block;
+    setValue(storageValue);
+  }, []);
 
-    const handleChange = () => {
-        if (value) {
-            set("block", false);
-            setValue(false);
-        } else {
-            set("block", true);
-            setValue(true);
-        }
+  const handleChange = () => {
+    if (value) {
+      set("block", false);
+      setValue(false);
+    } else {
+      set("block", true);
+      setValue(true);
+    }
 
-        location.reload();
-    };
+    location.reload();
+  };
 
-    return (
-        <li className={style.option_panel_setting}>
-            <span>{translate("Enable Illustration Refinement")}</span>
-            <input type="checkbox" onChange={handleChange} checked={value} />
-        </li>
-    );
+  return (
+    <li className={style.option_panel_setting}>
+      <span>{translate("Enable Illustration Filtering")}</span>
+      <input type="checkbox" onChange={handleChange} checked={value} />
+    </li>
+  );
 };
 
 export default FilterToggle;
